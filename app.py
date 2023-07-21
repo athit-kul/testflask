ff903f83cc81b20376d6a5309b91c107a6be1393
athit-kul <112455570+athit-kul@users.noreply.github.com>
2023-07-21 13:44:47 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from distutils.log import debug
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Web App with Python Flask!'
-app.run(port="8000")
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
